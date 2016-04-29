54ab1b25f2b98942cecf30ab7f06ac2a07794950
Rahim <rahim.dharssi@gmail.com>
2016-04-29 12:22:30 -0700

    Create basic app.js file

--- client/app.js
+++ client/app.js
@@ -1,4 +1,21 @@
-var test = 'test';
-
-
-
+angular.module('covey', [
+  // TODO: Add additional dependencies
+  'ngRoute',
+])
+.config(($routeProvider) => {
+  $routeProvider
+    .when('/', {
+      templateURL: 'views/index.html',
+      controller: 'loginController',
+    })
+    .when('/coveys', {
+      templateURL: 'views/coveys.html',
+      controller: 'coveysController',
+    })
+    .when('/covey', {
+      templateURL: 'views/covey.html',
+      controller: 'coveysController',
+    });
+})
+.run(() => {
+});

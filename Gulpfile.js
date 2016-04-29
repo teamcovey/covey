89b67695e413cc9b0e55bdd30a1064844a8d4e26
swfree <skye.free@gmail.com>
2016-04-29 11:59:40 -0700

    Edit gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var KarmaServer = require('karma').Server;
 var exec = require('child_process').exec;
 
-// the paths to our app files
 var paths = {
   // all our client app js files, not including 3rd party js files
   scripts: ['client/app.js', 'client/scripts/**/*.js'],
@@ -11,15 +10,14 @@ var paths = {
   test: ['specs/**/*.js']
 };
 
-// Run our karma tests 
-// TODO: Rename to 'client-test' ?
+// Runs client-side tests
 gulp.task('karma', function (done) {
   new KarmaServer({
     configFile: '/Users/skyefree/Documents/web-projects/hackreactor/covey/karma.conf.js'
   }, done).start();
 });
 
-// TODO: Rename to 'server-test' ?
+// Runs server-side tests
 gulp.task('mocha', function(done) {
   exec('mocha specs/server --compilers js:babel-register', function(err, stdout, stderr) {
     console.log(stdout);

e4f8cb37882484de6703c085964c793021ff5ba3
swfree <skye.free@gmail.com>
2016-04-30 14:24:58 -0700

    Add custom directives for covey detail, ride, and supply panels

--- client/scripts/controllers/covey.js
+++ client/scripts/controllers/covey.js
@@ -1,7 +1,39 @@
 angular.module('covey.covey', [])
 
-.controller('coveyController', function($scope) {
-  $scope.covey = "cheetah petting zoo";
+.controller('coveyController', function ($scope) {
+  /* Get Logged In User */
+  $scope.user = 'Skye';
+  
+  /* Covey Logic */
+  // on /covey load, use factory GET request for covey table events details
+  $scope.details = {
+    "id": 1, "eventName": "Camping With Friends", "time": "6PM", "location": "Yosemite, CA", "attendees": ["Freddie", "Rahim", "Toben", "Skye"] 
+  };
+  $scope.friends = $scope.details.attendees.join(', ');
+
+  // and GET rides table details for covey_id
+  $scope.rides = {
+    "rides": {
+      1: {
+        "id": 1, "covey_id": 1, "driverName": "Rahim", "timeToLeave": "3PM", 
+        "passengers": ["Friend1", "Friend2"]
+      },
+      2: {
+        "id": 2, "covey_id": 1, "driverName": "Freddie", "timeToLeave": "3PM", 
+        "passengers": ["Skye", "Toben"]
+      },
+    },
+  };
+
+  // and GET supplies table details for covey_id
+  $scope.supplies = {
+    "supplies": {
+      1: {
+        "id": 1, "covey_id": 1, "supplyName": "Beer", "suppliers": ["Rahim", "Freddie"]
+      },
+    },
+  };
+
   $scope.cars = ['skye'];
   $scope.supplies = ['chips'];
   $scope.expandRide = false;
@@ -17,11 +49,11 @@ angular.module('covey.covey', [])
 
   $scope.expandRides = () => {
     $scope.expandRide = !$scope.expandRide;
-  }
+  };
 
   $scope.expandSupplies = () => {
     $scope.expandSupply = !$scope.expandSupply;
-  }
+  };
 
   $scope.addSupply = () => {
     $scope.supplies.push('');
@@ -30,4 +62,19 @@ angular.module('covey.covey', [])
   $scope.addNewSupply = (supply) => {
     $scope.supplies[$scope.supplies.length - 1] = supply;
   };
-});
+})
+.directive('coveyDetails', () => (
+  {
+    templateUrl: 'views/coveyDetails.html',
+  }
+))
+.directive('rides', () => (
+  {
+    templateUrl: 'views/rides.html',
+  }
+))
+.directive('supplies', () => (
+  {
+    templateUrl: 'views/supplies.html',
+  }
+));

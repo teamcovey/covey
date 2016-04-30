13047d676457aacda1274898baa4af014abb9b2b
swfree <skye.free@gmail.com>
2016-04-29 21:59:34 -0700

    Add expandable, editable panels to covey pg

--- client/scripts/controllers/covey.js
+++ client/scripts/controllers/covey.js
@@ -2,4 +2,32 @@ angular.module('covey.covey', [])
 
 .controller('coveyController', function($scope) {
   $scope.covey = "cheetah petting zoo";
+  $scope.cars = ['skye'];
+  $scope.supplies = ['chips'];
+  $scope.expandRide = false;
+  $scope.expandSupply = false;
+
+  $scope.addCar = () => {
+    $scope.cars.push('');
+  };
+
+  $scope.addNewCar = (car) => {
+    $scope.cars[$scope.cars.length - 1] = car;
+  };
+
+  $scope.expandRides = () => {
+    $scope.expandRide = !$scope.expandRide;
+  }
+
+  $scope.expandSupplies = () => {
+    $scope.expandSupply = !$scope.expandSupply;
+  }
+
+  $scope.addSupply = () => {
+    $scope.supplies.push('');
+  };
+
+  $scope.addNewSupply = (supply) => {
+    $scope.supplies[$scope.supplies.length - 1] = supply;
+  };
 });

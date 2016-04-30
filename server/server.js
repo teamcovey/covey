15e5f754867a65adff5318833ef03b7e84589c38
tobensg <tobensg@gmail.com>
2016-04-29 17:22:11 -0700

    setting up testing for the back end.  Basic http code tests in place for endpoints.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const app = require('./routers/router.js');
 
-console.log('Covey is listening on 8080');
-
 app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;

61ebb45ab8199f8732eb3896b6a85a34eb041912
Fred Ryder <frederickryder@gmail.com>
2016-04-30 17:05:49 -0700

    Code clean-up: in routes.js: userCheck... -> auth

--- server/routers/router.js
+++ server/routers/router.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 
 // Authentication
 const passport = require('../config/passport.js');
-const userCheck = require('connect-ensure-login');
+const auth = require('connect-ensure-login').ensureLoggedIn('/api/auth');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
@@ -23,17 +23,17 @@ app.use(passport.session());
 // Routes: app
 app.get('/', route.getUsage);
 
-app.get('/api/user', userCheck.ensureLoggedIn('/api/auth'), route.getUser);
+app.get('/api/user', auth, route.getUser);
 
-app.get('/api/coveys', userCheck.ensureLoggedIn('/api/auth'), route.getAllCoveys);
+app.get('/api/coveys', auth, route.getAllCoveys);
 
-app.post('/api/coveys', userCheck.ensureLoggedIn('/api/auth'), route.addCovey);
+app.post('/api/coveys', auth, route.addCovey);
 
-app.delete('/api/coveys/:id', userCheck.ensureLoggedIn('/api/auth'), route.removeCovey);
+app.delete('/api/coveys/:id', auth, route.removeCovey);
 
-app.put('/api/coveys/:id', userCheck.ensureLoggedIn('/api/auth'), route.updateCovey);
+app.put('/api/coveys/:id', auth, route.updateCovey);
 
-app.get('/api/coveys/:id', userCheck.ensureLoggedIn('/api/auth'), route.getCovey);
+app.get('/api/coveys/:id', auth, route.getCovey);
 
 app.post('/api/signup', route.signup);
 

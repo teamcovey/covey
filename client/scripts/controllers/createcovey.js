2465f86cc5a505db171a2ae28cc2da9dde0d65bc
Rahim <rahim.dharssi@gmail.com>
2016-05-02 20:59:03 -0700

    Add additional comments to controller code, redirect user to new covey page upon creation

--- client/scripts/controllers/createcovey.js
+++ client/scripts/controllers/createcovey.js
@@ -1,6 +1,6 @@
 const createCovey = angular.module('createCovey', ['covey.services']);
 
-createCovey.controller('createCoveyController', ($scope, $rootScope, coveysFactory) => {
+createCovey.controller('createCoveyController', ($scope, $rootScope, $location, coveysFactory) => {
   // Visibility of modal
   $scope.visible = false;
 
@@ -22,6 +22,10 @@ createCovey.controller('createCoveyController', ($scope, $rootScope, coveysFacto
     $scope.errorVisible = !$scope.errorVisible;
   };
 
+  /*
+   * Submits new covey to the server. If successful (201 response),
+   * it will close the modal and angular will redirect the bew covey
+  */
   $scope.submitCovey = () => {
     const coveyData = {
       name: $scope.eventName,
@@ -36,10 +40,14 @@ createCovey.controller('createCoveyController', ($scope, $rootScope, coveysFacto
           $scope.toggleErrorVisibility();
         } else {
           $scope.toggleModalVisibility();
+          // TODO: Will need to update based on server implementaiton
+          const newCoveyId = response.id;
+          // Redirects the user to the new covey page
+          $location.path('/coveys/'.concat(newCoveyId));
         }
       });
   };
-
+  // Toggles visibility of the modal whenever the create covey button is clicked
   $rootScope.$on('toggleCreateCoveyModal', () => {
     $scope.toggleModalVisibility();
   });
--- client/scripts/controllers/hamburger.js
+++ client/scripts/controllers/hamburger.js
@@ -2,7 +2,7 @@ const hamburger = angular.module('hamburger', []);
 
 hamburger.controller('hamburgerController', ($scope) => {
   $scope.showMenu = false;
-
+  // Toggles visibility of hamburger meny
   $scope.toggleMenu = () => {
     $scope.showMenu = !$scope.showMenu;
   };

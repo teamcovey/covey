768132c6d8f8a31387cb43f7001b225856fa96e6
Rahim <rahim.dharssi@gmail.com>
2016-05-05 19:47:44 -0700

    Adjust coveysFactory functions to work with unit tests

--- client/scripts/services/services.js
+++ client/scripts/services/services.js
@@ -4,26 +4,26 @@ const coveyServices = angular.module('covey.services', []);
 coveyServices.factory('coveysFactory', function ($http) {
   return ({
     // Gets the full set of coveys for this user
-    getCoveys: () => {
-      const requestUrl = '/api/coveys';
+    getCoveys: (endpoint) => {
+      const requestUrl = endpoint || '/api/coveys';
       return $http.get(requestUrl)
         .then((response) => response, (error) => error);
     },
     // Creates a new covey for this user
-    postCovey: (coveyData) => {
-      const requestUrl = '/api/coveys';
+    postCovey: (coveyData, endpoint) => {
+      const requestUrl = endpoint || '/api/coveys';
       return $http.post(requestUrl, coveyData)
         .then((response) => response, (error) => error);
     },
     // Gets a single covey for this user
-    getCovey: (coveyId) => {
-      const requestUrl = `/api/coveys/${coveyId}`;
+    getCovey: (coveyId, endpoint) => {
+      const requestUrl = endpoint || `/api/coveys/${coveyId}`;
       return $http.get(requestUrl)
         .then((response) => response, (error) => error);
     },
     // Updates a covey based on user changes
-    putCovey: (coveyData, coveyId) => {
-      const requestUrl = `/api/coveys/${coveyId}`;
+    putCovey: (coveyData, coveyId, endpoint) => {
+      const requestUrl = endpoint || `/api/coveys/${coveyId}`;
       const requestData = {
         covey: coveyData,
       };
@@ -33,8 +33,8 @@ coveyServices.factory('coveysFactory', function ($http) {
         .then((response) => response, (error) => error);
     },
     // Deletes a covey, based on user request
-    deleteCovey: (coveyId) => {
-      const requestUrl = `/api/coveys/${coveyId}`;
+    deleteCovey: (coveyId, endpoint) => {
+      const requestUrl = endpoint || `/api/coveys/${coveyId}`;
       return $http.delete(requestUrl)
         .then((response) => response, (error) => error);
     },

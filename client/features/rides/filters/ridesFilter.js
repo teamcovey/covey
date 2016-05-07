52567e1582ad4fe0b299f43cf24655c2942b60fc
swfree <skye.free@gmail.com>
2016-05-07 10:11:42 -0700

    Remove stray comments from ridesFilter file

--- client/features/rides/filters/ridesFilter.js
+++ client/features/rides/filters/ridesFilter.js
@@ -21,36 +21,3 @@ angular.module('covey.rides')
     }
   });
 });
-
-
-//   return (attendees, rides) => {
-//     let allPassengers = [];
-//     rides.forEach((ride) => {
-//       allPassengers = allPassengers.concat(ride.riders);
-//     });
-//     return attendees.filter((attendee) => {
-//       let result = true;
-//       allPassengers.forEach((currentPassenger) => {
-//         if (currentPassenger.user_id === attendee.user_id) {
-//           result = false;
-//         }
-//       });
-//       return result;
-//     });
-//   };
-// // The memoized filter function
-// angular.module("TestApp.filters", [])
-//   .filter("numbersToObjects", function(){
-//       //takes an array of integers and returns an array of objects with the numbers published under the specified property
-//       return _.memoize(function(numbers, propertyToPublishNumberUnder){
-//         propertyToPublishNumberUnder = propertyToPublishNumberUnder || "val";
-//         var objects = [], object;
-//         angular.forEach(numbers, function(number, index){
-//           object = {};
-//           object[propertyToPublishNumberUnder] = number
-//           objects.push(object);
-//         });
-//         return objects;  
-//       });
-//     //}
-//   }) 

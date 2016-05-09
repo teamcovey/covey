08bbe1cb832a2a8814e55cb0b923bd28f657ff17
Fred Ryder <frederickryder@gmail.com>
2016-05-09 15:12:19 -0700

    Remove unused of file system folders

--- client/features/welcome/directives/welcomeDirective.js
+++ client/features/welcome/directives/welcomeDirective.js
@@ -3,6 +3,5 @@ angular.module('welcome')
 .directive('welcomeDirective', () => {
   return {
     templateUrl: 'features/welcome/views/welcome.html',
-    controller: 'welcomeController',
   };
 });

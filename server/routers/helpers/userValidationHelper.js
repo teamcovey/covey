db7db24e4f0467efbc062e42a1ccdb4a72fe5175
Rahim <rahim.dharssi@gmail.com>
2016-05-09 15:39:15 -0700

    Add comment to user validator function

--- server/routers/helpers/userValidationHelper.js
+++ server/routers/helpers/userValidationHelper.js
@@ -1,6 +1,10 @@
 const knex = require('../../config/config.js').knex;
 
-const isValidUser = (userId, coveyId) => {
+/*
+ * Checks if the user/covey pair exists in the join table. If the pair does not
+ * exist, the user is considered inValid and will not be able to make changes to the covey
+ */
+exports.isValidUser = (userId, coveyId) => {
   return knex('coveys_users')
     .where({
       user_id: userId,
@@ -11,5 +15,3 @@ const isValidUser = (userId, coveyId) => {
       return false;
     });
 };
-
-module.exports = isValidUser;

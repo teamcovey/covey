e1b0a1ea20e9bd2674558fb6870db0c61df152b1
tobensg <tobensg@gmail.com>
2016-05-09 17:05:17 -0700

    can now remove friends the page reloads without making a new request to the db.

--- client/features/friends/controllers/friendsController.js
+++ client/features/friends/controllers/friendsController.js
@@ -27,9 +27,28 @@ angular.module('friends', ['friends.services', 'userId.services'])
   // Setting to testData for now, will update once server isrunning
   $scope.friends = friendTestData;
   // Routes user to the specified covey (based on coveyId)
-  // $scope.goToCovey = (coveyId) => {
-  //   $location.path(`/coveys/${coveyId}`);
-  // };
+  $scope.removeFriend = (friendId) => {
+    let friendIndex = $scope.friends.length;
+    friendsFactory
+      .removeFriend(friendId)
+      .then((response) => {
+        console.log('got response in removeFriends...', response.data);
+        const data = response.data;
+        if (data.success && data.success === true) {
+          for (let i = 0; i < friendIndex; i++) {
+            if ($scope.friends[i].id === friendId) {
+              friendIndex = i;
+            }
+          }
+          $scope.friends.splice(friendIndex, 1);
+          if ($scope.friends.length === 0) {
+            $scope.hasFriends = 'false';
+          }
+          console.log('make the page reload?');
+          // $scope.getFriends();
+        }
+      });
+  };
   /*
    *  Triggered whenever the 'create' button is clicked.
    *  Will toggle visibility of the create covey modal.
@@ -51,26 +70,22 @@ angular.module('friends', ['friends.services', 'userId.services'])
   //   }
   // };
   /*
-   * Gets all coveys from the server (for the current user)
+   * Gets all friends from the server (for the current user)
    */
   $scope.getFriends = () => {
     friendsFactory
       .getFriends()
       .then((response) => {
-        console.log('got response in getFriends...', response.data);
         const status = response.status;
         const data = response.data;
         if (status !== 200) {
-          // When hasCoveys is equal to 'error' a different view will be displayed on the page
+          // When hasFriends is equal to 'error' a different view will be displayed on the page
           $scope.hasFriends = 'error';
-          console.log('error in getFriends in friendsController');
         } else {
           if (data.length === 0) {
-            // When hasCoveys is equal to 'false' a different view will be displayed on the page
+            // When hasFriends is equal to 'false' a different view will be displayed on the page
             $scope.hasFriends = 'false';
-            console.log('no friends found in friendsController');
           } else {
-            console.log('setting friends to data in friendsController');
             $scope.friends = data;
             // $scope.sortFriendsByOwnershipStatus($scope.friends);
           }
@@ -78,8 +93,7 @@ angular.module('friends', ['friends.services', 'userId.services'])
       });
   };
   /*
-   * Automatically gets all coveys on page load.
-   * TODO: uncomment the below line once server is running.
+   * Automatically gets all friends on page load.
    */
   $scope.getFriends();
 });

ca177c81400bade05c18083c37df3203345334aa
swfree <skye.free@gmail.com>
2016-05-13 16:51:41 -0700

    Indicate when client disconnects from socket

--- server/routers/router.js
+++ server/routers/router.js
@@ -12,6 +12,10 @@ io.on('connection', (socket) => {
   socket.on('echo', (data) => {
     io.sockets.emit('message', data);
   });
+
+  socket.on('disconnect', () => {
+    console.log('Got disconnect!');
+  });
 });
 
 app.use((req, res, next) => {

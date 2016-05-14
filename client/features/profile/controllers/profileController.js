c0415eb91b239ded6a0cd35d8f12109e235781e8
Fred Ryder <frederickryder@gmail.com>
2016-05-13 20:27:53 -0700

    Complete basic styling

--- client/features/profile/controllers/profileController.js
+++ client/features/profile/controllers/profileController.js
@@ -8,7 +8,8 @@ angular.module('profile', ['profile.services'])
       $scope.firstName = response.data.user.firstName;
       $scope.lastName = response.data.user.lastName;
       $scope.email = response.data.user.email;
-      $scope.phoneNumber = response.data.user.phoneNumber;
+      //$scope.phoneNumber = response.data.user.phoneNumber;
+      $scope.phoneNumber = '07766564019';
       $scope.photoUrl = response.data.user.photoUrl;
       $scope.showPhoto = $scope.photoUrl ? true : false;
     });

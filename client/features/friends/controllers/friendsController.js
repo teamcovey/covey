659758ffe4d6029e0efcb6bb7162be368ebd91aa
tobensg <tobensg@gmail.com>
2016-05-17 13:59:21 -0700

    added confirmation for removing friends

--- client/features/friends/controllers/friendsController.js
+++ client/features/friends/controllers/friendsController.js
@@ -82,6 +82,13 @@ angular.module('friends', ['friends.services', 'userId.services'])
   $scope.hideSearch = () => {
     $scope.showFriends = 'true';
   }
+
+  $scope.confirmDelete = (friendName, friendId) => {
+    // using a pop confirm window to make sure people cant accidentally remove friends
+    if (confirm('Would you like to remove ' + friendName + ' from your friends list?') == true) {
+        $scope.removeFriend(friendId);
+    } 
+  }
   /*
    * Automatically gets all friends on page load.
    */

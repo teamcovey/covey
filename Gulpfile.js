898ee9dcbe1c9a3e52c4056c7f599fb45db7aca0
tobensg <tobensg@gmail.com>
2016-05-18 14:47:51 -0700

    added concat minify replace for gulp go-prod

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var KarmaServer = require('karma').Server;
 var exec = require('child_process').exec;
+var babel = require('gulp-babel');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var processhtml = require('gulp-processhtml');
 
 var paths = {
   // all our client app js files, not including 3rd party js files
-  scripts: ['client/app.js', 'client/scripts/**/*.js'],
+  scripts: ['client/app.js', 'client/scripts/**/*.js', 'client/features/**/*.js'],
   html: ['client/views/*.html', 'client/index.html'],
   styles: ['client/styles/*.css'],
   test: ['specs/**/*.js']
@@ -28,5 +33,31 @@ gulp.task('mocha', function(done) {
   });
 });
 
+gulp.task('compress', function() {
+  return gulp.src(paths.scripts)
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(uglify())
+    .on('error', function(err) {
+      console.log('error was ', err);
+    })
+    .pipe(gulp.dest('client/dist'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('client/styles/*.css')
+    .pipe(concat("all.css"))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('client/dist'));
+});
+
+gulp.task('min-index', function () {
+    return gulp.src('client/index.html')
+               .pipe(processhtml())
+               .pipe(gulp.dest('client'));
+});
+
 // TODO: npm test should call gulp test which should do tasks mocha, karma
 gulp.task('test', ['mocha', 'karma']);
+
+gulp.task('go-prod', ['compress', 'minify-css', 'min-index']);

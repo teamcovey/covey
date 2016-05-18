c8cce4e5bfe54666dff45e8897869fef51022508
swfree <skye.free@gmail.com>
2016-05-17 21:55:16 -0700

    Add scrollable splash home page

--- client/features/index/controllers/indexController.js
+++ client/features/index/controllers/indexController.js
@@ -1,6 +1,16 @@
 angular.module('covey.index', ['ngCookies'])
 
 .controller('indexController', function ($scope, $cookies, indexService) {
+  
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+
   // document.cookie = 'new_user = false';
   $cookies.remove('new_user');
   $scope.setCookie = () => {

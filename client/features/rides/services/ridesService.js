4c08baecb437606189d9e3c9423be001e75b5f5e
tobensg <tobensg@gmail.com>
2016-05-20 13:59:34 -0700

    remove log messages.  end of testing.

--- client/features/rides/services/ridesService.js
+++ client/features/rides/services/ridesService.js
@@ -38,7 +38,6 @@ angular.module('covey.rides')
   };
 
   this.getAllRiders = (rideId) => {
-    console.log('SRVC: getAllRiders: ', rideId);
     return $http.get(`/api/riders/${rideId}`)
     .then((riders) => riders.data, (error) => {
       console.error(error);
@@ -46,7 +45,6 @@ angular.module('covey.rides')
   };
 
   this.addRide = (newRide) => {
-    console.log('SRVC: addRide: ', newRide);
     return $http.post('/api/rides', newRide)
       .then((response) => response.data, (error) => {
         console.error(error);
@@ -54,7 +52,6 @@ angular.module('covey.rides')
   };
 
   this.updateRide = (updateRide) => {
-    console.log('SRVC: updateRide: ', updateRide);
     return $http.put(`/api/rides/${updateRide.carId}`, updateRide)
       .then((response) => response, (error) => {
         console.error(error);
@@ -70,7 +67,6 @@ angular.module('covey.rides')
 
 
   this.addPassenger = (rideId, userId) => {
-    console.log('SRVC: addPassenger: ', rideId);
     return $http.post(`/api/riders/${rideId}/${userId}`, { coveyId: $routeParams.coveyId })
       .then((response) => response, (error) => {
         console.error(error);
